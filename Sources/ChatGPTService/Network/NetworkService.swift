d523bf042033f2de7c4a5ea6a4237c109ce3bde2
Yeskendir Salgara <36730580+sorunokoe@users.noreply.github.com>
2024-06-14 17:14:19 +0200

    add macOS support and convert to async/await Networking

--- Sources/ChatGPTService/Network/NetworkService.swift
+++ Sources/ChatGPTService/Network/NetworkService.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by SALGARA, YESKENDIR on 26.03.24.
 //
@@ -8,35 +8,40 @@
 import Foundation
 
 final class NetworkService {
-    
-    func request<T: Decodable>(target: Target) async -> T? {
-        await withCheckedContinuation { continuation in
-            let url = URL(string: target.baseURL + target.path)!
-            var request = URLRequest(url: url)
-            request.allHTTPHeaderFields = target.header
-            request.httpBody = try? JSONSerialization.data(withJSONObject: target.body)
-            request.httpMethod = target.method.rawValue
-            
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                guard let data = data, error == nil else {
-                    print("🔴" + " " + (error?.localizedDescription ?? "No data"))
-                    continuation.resume(returning: nil)
-                    return
-                }
-                #if DEBUG
-                print(String(data: data, encoding: .utf8))
-                #endif
-                do {
-                    let response = try JSONDecoder().decode(T.self, from: data)
-                    continuation.resume(returning: response)
-                } catch {
-                    print("🔴 Decoding failed: \(error)")
-                    continuation.resume(returning: nil)
-                }
-            }
-            
-            task.resume()
-        }
+    private let session: URLSession
+
+    init() {
+        let configuration = URLSessionConfiguration.default
+        configuration.timeoutIntervalForRequest = 30.0
+        configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+
+        self.session = URLSession(configuration: configuration)
     }
-    
+
+    func request<T: Decodable>(target: Target) async -> T? {
+        let url = URL(string: target.baseURL + target.path)!
+        var request = URLRequest(url: url)
+        request.allHTTPHeaderFields = target.header
+        request.httpBody = try? JSONSerialization.data(withJSONObject: target.body)
+        request.httpMethod = target.method.rawValue
+
+        do {
+            let (data, _) = try await session.data(for: request)
+            #if DEBUG
+            print(String(data: data, encoding: .utf8))
+            #endif
+            do {
+                let response = try JSONDecoder().decode(T.self, from: data)
+                return response
+            } catch {
+                print("🔴 Decoding failed: \(error)")
+            }
+        } catch {
+            #if DEBUG
+            print(String(describing: error))
+            #endif
+            return nil
+        }
+        return nil
+    }
 }
